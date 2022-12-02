c0de9c17b37edf6a3b0d4c7b37a03e74079d970e
Mariana Rodrigues <rodriguesmariana1106@gmail.com>
2022-12-01 23:21:44 -0300

    questoes 03, 11, 12

---
+++ 11.js
@@ -0,0 +1,16 @@
+//  novo array com núm  que estejam entre 10 e 20,incluindo eles. ok
+//  ou que sejam maiores que 100. ok
+//  imprima a variável que guarda o novo array.
+
+const original = [5, 7, 13, 17, 26, 34, 118, 245];
+
+
+arrayFiltrados = [];
+
+for (numero of original) {
+    if ((numero >= 10 && numero <= 20) || numero > 100) {
+        arrayFiltrados.push(numero);
+    }
+}
+
+console.log(arrayFiltrados);
